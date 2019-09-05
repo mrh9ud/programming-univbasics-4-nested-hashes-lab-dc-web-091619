144ddb72b694cc32dcb313597577ba6617b205a3
Matthew Heavner <mrh9ud@virginia.edu>
2019-09-05 20:04:20 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -129,7 +129,8 @@ def adding_to_dennis
         }
      }
   
-  programmer_hash[:dennis_ritchie][:languages]
+  programmer_hash[:dennis_ritchie][:languages][1] = "Assembly"
+  programmer_hash
 end
 
 
